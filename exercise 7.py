710a212efe95e897c6c8a2488d9cc85750312ca6
eestey <eestey@students.mchenry.edu>
2016-09-29 17:35:36 -0500

    Add files via upload

---
+++ exercise 7.py
@@ -0,0 +1,27 @@
+def is_triple_double(word):
+    i = 0
+    count = 0
+    while i < len(word)-1:
+        if word[i] == word[i+1]:
+            count += 1
+            if count == 3:
+                return True
+            i += 2
+        else:
+            count = 0
+            i += 1
+    return False
+
+
+def find_triple_double():
+    fin = open('C:\\Users\PC2\Documents\PRG105\words.txt')
+    for line in fin:
+        word = line.strip()
+        if is_triple_double(word):
+            print word
+
+
+print 'Here are all the words in the list that have'
+print 'three consecutive double letters.'
+find_triple_double()
+print ''
